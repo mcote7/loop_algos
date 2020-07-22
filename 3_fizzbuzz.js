eaec652f88a5b04c95f27522468ed705fe8503d0
Michael Cote <michaelcote7@outlook.com>
2020-07-21 19:47:32 -0700

    .

--- 3_fizzbuzz.js
+++ 3_fizzbuzz.js
@@ -6,8 +6,17 @@
 
 //The expected output will be 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz .... etc 
 
-
+function fizzBuzz(n) {
+  for(let i=1; i <= n; i++){
+    if (i % 15 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
 
 
 
 //YOUR CODE HERE
+
+
